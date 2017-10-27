a39cc207dc792b0914d988bca875aaae4dd24a18
Minghe Huang <h.minghe@gmail.com>
2017-10-27 09:08:59 +0000

    clean up

--- install.sh
+++
@@ -1,7 +0,0 @@
-go get -v \
-    github.com/docker/docker/client \
-    github.com/docker/go-connections \
-    github.com/gorilla/websocket \
-    github.com/jhoonb/archivex \
-    github.com/phayes/freeport \
-    github.com/rs/xid \
