2e586267c36a47dcc5a781a055e7c555de0d6928
Minghe Huang <h.minghe@gmail.com>
2017-10-31 20:45:59 +0800

    clean up

--- server/worker/worker.go
+++ server/worker/worker.go
@@ -2,14 +2,12 @@ package worker
 
 import (
 	"archive/tar"
-	"bufio"
 	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"path"
 	"path/filepath"
 	"strconv"
@@ -127,34 +125,6 @@ func CopyDir(src string, dst string) (err error) {
 	}
 
 	return
-}
-
-func runCmd(cmdName string, cmdArgs []string) {
-	cmd := exec.Command(cmdName, cmdArgs...)
-	cmdReader, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
-		os.Exit(1)
-	}
-
-	scanner := bufio.NewScanner(cmdReader)
-	go func() {
-		for scanner.Scan() {
-			fmt.Printf("%s | %s\n", cmdName, scanner.Text())
-		}
-	}()
-
-	err = cmd.Start()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
-		os.Exit(1)
-	}
-
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
-		os.Exit(1)
-	}
 }
 
 func initWorkDirectory(lang string, dir string) {
