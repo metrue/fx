55476a4f1cbf31131107bb2eef37fad766d75d43
Minghe Huang <h.minghe@gmail.com>
2017-11-16 00:14:46 +0800

    use correct varible

--- config/config.go
+++ config/config.go
@@ -1,11 +1,12 @@
 package config
 
 import (
-	"path"
-	"os"
 	"flag"
+	"os"
+	"path"
 )
 
 var CacheDir = path.Join(os.Getenv("HOME"), ".fx/")
 var RemoteImagesUrl = "https://raw.githubusercontent.com/metrue/fx/master/images.zip"
 var ServerAddr = flag.String("addr", "localhost:8080", "http service address")
+var ServePort = 8080
