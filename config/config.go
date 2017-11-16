45730e05cd599370f0a2777e9ad25dd9c1550a70
Minghe Huang <h.minghe@gmail.com>
2017-11-16 11:48:45 +0800

    refractor config

--- config/config.go
+++ config/config.go
@@ -1,12 +1,16 @@
 package config
 
 import (
-	"flag"
 	"os"
 	"path"
 )
 
-var CacheDir = path.Join(os.Getenv("HOME"), ".fx/")
-var RemoteImagesUrl = "https://raw.githubusercontent.com/metrue/fx/master/images.zip"
-var ServerAddr = flag.String("addr", "localhost:8080", "http service address")
-var ServePort = 8080
+const Server = map[string]string{
+	"host": "localhost",
+	"port": "8080",
+}
+
+const Client = map[string]string{
+	"CacheDir":        path.Join(os.Getenv("HOME"), ".fx/"),
+	"RemoteImagesUrl": "https://raw.githubusercontent.com/metrue/fx/master/images.zip",
+}
