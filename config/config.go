50a41579b66fc12a512c710ce3dc294dcb5d4796
Minghe Huang <h.minghe@gmail.com>
2017-11-16 11:57:16 +0800

    fix sytax error

--- config/config.go
+++ config/config.go
@@ -5,12 +5,12 @@ import (
 	"path"
 )
 
-const Server = map[string]string{
+var Server = map[string]string{
 	"host": "localhost",
 	"port": "8080",
 }
 
-const Client = map[string]string{
+var Client = map[string]string{
 	"CacheDir":        path.Join(os.Getenv("HOME"), ".fx/"),
 	"RemoteImagesUrl": "https://raw.githubusercontent.com/metrue/fx/master/images.zip",
 }
