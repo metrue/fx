9c07efd663a0eca7b3b2e124d11a3ad5e79381ac
matbesancon <mathieu.besancon@gmail.com>
2017-11-19 18:27:18 -0500

    added julia image and example function

---
+++ images/julia/app.jl
@@ -0,0 +1,17 @@
+using HttpServer
+import JSON
+import Unmarshal
+
+include("fx.jl")
+
+srv = Server() do req::Request, res::Response
+    if length(req.data) == 0
+        res.status = 400
+    else
+        parsed_data = JSON.Parser.parse(join([Char(v) for v in req.data]))
+        data = Unmarshal.unmarshal(Input, parsed_data)
+        string(fx(data))
+    end
+end
+
+run(srv, 8080)
