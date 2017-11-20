cc43861ca02c1863cd22d21d9afc66204a84a03c
matbesancon <mathieu.besancon@gmail.com>
2017-11-19 19:34:13 -0500

    adding documentation to core functions

--- config/config.go
+++ config/config.go
@@ -5,11 +5,13 @@ import (
 	"path"
 )
 
+// Server contains the server configuration information
 var Server = map[string]string{
 	"host": "localhost",
 	"port": "8080",
 }
 
+// Client contains the local and remote paths to fetch cached images
 var Client = map[string]string{
 	"cache_dir":         path.Join(os.Getenv("HOME"), ".fx/"),
 	"remote_images_url": "https://raw.githubusercontent.com/metrue/fx/master/images.zip",
--- server/handlers/list.go
+++ server/handlers/list.go
@@ -7,16 +7,15 @@ import (
 	"github.com/docker/docker/client"
 )
 
+// List returns the list of running services
 func List() []types.Container {
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
 	}
-
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil {
 		panic(err)
 	}
-
 	return containers
 }
