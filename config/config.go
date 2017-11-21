b03a57f4cce6a208183d57fb62f8aa8c5824daf9
Minghe Huang <h.minghe@gmail.com>
2017-11-21 18:16:03 +0800

    move ext map language to config

--- config/config.go
+++ config/config.go
@@ -16,3 +16,12 @@ var Client = map[string]string{
 	"cache_dir":         path.Join(os.Getenv("HOME"), ".fx/"),
 	"remote_images_url": "https://raw.githubusercontent.com/metrue/fx/master/images.zip",
 }
+
+var ExtLangMap = map[string]string {
+	".js":  "node",
+	".go":  "go",
+	".rb":  "ruby",
+	".py":  "python",
+	".php": "php",
+	".jl":  "julia",
+}
