be9472a53cfd894cdbfaaa12d0331a05cacd8dd9
Minghe Huang <h.minghe@gmail.com>
2017-11-25 21:42:28 +0800

    clean a little

--- images/R/packages.R
+++ images/R/packages.R
@@ -1,2 +1,1 @@
 install.packages(c("jug", "jsonlite"), repos="http://cran.us.r-project.org", dependencies=TRUE)
-# install.packages(c("jsonlite"), repos="http://cran.us.r-project.org", dependencies=TRUE)
