84b8778ddf189353a66f1fb6c9ea0458b557c0d6
Minghe Huang <h.minghe@gmail.com>
2017-12-09 09:47:31 +0800

    add install script

---
+++ bin/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# v0.0.1
+# https://github.com/metrue/fx/releases/download/v0.0.1/fx_0.0.1_checksums.txt
+# https://github.com/metrue/fx/releases/download/v0.0.1/fx_0.0.1_macOS_64-bit.tar.gz
+# https://github.com/metrue/fx/releases/download/v0.0.1/fx_0.0.1_Tux_64-bit.tar.gz
+# https://github.com/metrue/fx/releases/download/v0.0.1/fx_0.0.1_windows_64-bit.tar.gz
+
+get_package_url() {
+    local label=$1
+    curl -s https://api.github.com/repos/metrue/fx/releases/latest | grep browser_download_url | awk -F'"' '{print $4}' | grep ${label}
+}
+
+download_and_unzip() {
+    local url=$1
+    curl -OL ${url} | tar xvz
+}
+
+url=""
+if [ "$(uname)" == "Darwin" ]; then
+    url=$(get_package_url 'macOS')
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    url=$(get_package_url 'Tux')
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+#     # TODO no support
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # TODO no support
+fi
+
+if [ ${url}"X" != "X" ];then
+    download_and_unzip ${url}
+fi
