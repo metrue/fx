23a27a89efdcc225821a1c954c984da66bbcaf7b
Minghe Huang <h.minghe@gmail.com>
2017-12-31 17:47:18 +0800

    fix dockerfile syntax issue

--- api/service/up_dockerfile_test.go
+++ api/service/up_dockerfile_test.go
@@ -2,8 +2,8 @@ package service
 
 import (
 	"context"
-	"testing"
 	"fmt"
+	"testing"
 	"time"
 
 	"github.com/metrue/fx/api"
@@ -14,30 +14,35 @@ func createDockerfile(client api.FxServiceClient) (*api.UpMsgMeta, error) {
 	upReq := &api.UpDockerfileRequest{
 		Dockerfiles: []*api.DockerfileMeta{
 			&api.DockerfileMeta{
-				Content: fmt.Sprintf(`FROM ubuntu \n
-RUN apt-get install -y ruby rubygems git\n
-RUN git clone https://github.com/luisbebop/docker-sinatra-hello-world.git /opt/sinatra/ \n
-RUN gem install bundler \n
-EXPOSE 3000 \n
-RUN echo \"%d\"
-RUN cd /opt/sinatra && git pull && bundle install \n
-CMD [\"/usr/local/bin/foreman\",\"start\",\"-d\",\"/opt/sinatra\"]`, time.Now().Unix()),
+				Content: fmt.Sprintf(`FROM ubuntu
+RUN apt-get update
+RUN apt-get install -y ruby rubygems git
+RUN git clone https://github.com/luisbebop/docker-sinatra-hello-world.git /opt/sinatra/
+RUN gem install bundler
+EXPOSE 3000
+RUN echo "%d"
+RUN cd /opt/sinatra && git pull && bundle install
+CMD ["/usr/local/bin/foreman","start","-d","/opt/sinatra"]`, time.Now().Unix()),
 			},
 		},
 	}
 
+	fmt.Println("to Up 1")
 	upRes, err := client.UpDockerfile(ctx, upReq)
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println("to Up 2")
 
 	if len(upRes.Instances) != 1 {
 		return nil, fmt.Errorf("UpDockerfile response should have one instance, found %d", len(upRes.Instances))
 	}
+	fmt.Println("to Up 3")
 	if upRes.Instances[0].Error != "" {
 		return nil, fmt.Errorf("Up error: %s", upRes.Instances[0].Error)
 	}
 
+	fmt.Println("to Up 4")
 	return upRes.Instances[0], nil
 }
 
