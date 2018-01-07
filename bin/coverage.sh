8a2ba8172c5b60262273d4380ebb17113209aa0b
Minghe Huang <h.minghe@gmail.com>
2018-01-07 15:33:33 +0800

    enable test of api/service

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -3,10 +3,7 @@
 set -e
 echo "mode: atomic\n" > coverage.txt
 
-# TODO it's very weird, tests of api/service always fails on CirCleCI environment,
-# but works on local
-# for d in ./image ./api/service; do
-for d in ./image; do
+for d in ./image ./api/service; do
     go test -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out | grep -v "mode: atomic" >> coverage.txt
