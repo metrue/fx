ddbfc07227cc346f6ce777153bbac31655ad8fc1
Minghe Huang <h.minghe@gmail.com>
2018-06-19 22:53:15 +0800

    golang built in flag sucks

--- fx.go
+++ fx.go
@@ -1,88 +1,62 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"log"
 	"os"
 
-	"github.com/metrue/fx/commands"
 	"github.com/metrue/fx/server"
+	"github.com/urfave/cli"
 )
 
-const version string = "0.0.2"
-
-const usage = `Usage:
-  $ fx up   func1 func2 ...       deploy a function or a group of functions
-  $ fx down func1 func2 ...       destroy a function or a group of functions
-  $ fx list                       list deployed services
-  $ fx serve                      manage fx server
-  $ fx --version                  show current version of f(x)
-`
-
-func versionAndExit() {
-	fmt.Println(version)
-	os.Exit(0)
-}
-
-func helpAndExit() {
-	fmt.Print(usage)
-	os.Exit(0)
-}
-
-func parseFlags() (version bool, help bool, verbose bool) {
-	helpPtr := flag.Bool(
-		"help",
-		false,
-		"Help information.",
-	)
-
-	versionPtr := flag.Bool(
-		"version",
-		false,
-		"Version information.",
-	)
-
-	verbosePtr := flag.Bool(
-		"verbose",
-		false,
-		"Make the operation more talkative",
-	)
-
-	flag.Parse()
-
-	version = *versionPtr
-	help = *helpPtr
-	verbose = *verbosePtr
-
-	return version, help, verbose
-}
-
 func main() {
-	nArgs := len(os.Args)
-	if nArgs < 2 {
-		helpAndExit()
+	app := cli.NewApp()
+	app.Name = "fx"
+	app.Usage = "make function as a service"
+	app.Version = "0.0.2"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "serve",
+			Aliases: []string{"s"},
+			Usage:   "manage fx server",
+			Action: func(c *cli.Context) error {
+				fmt.Println("serve: ", c.Args().First())
+				server.Start(verbose)
+				return nil
+			},
+		},
+		{
+			Name:    "up",
+			Aliases: []string{"u"},
+			Usage:   "deploy a function or a group of functions",
+			Action: func(c *cli.Context) error {
+				fmt.Println("up: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "down",
+			Aliases: []string{"d"},
+			Usage:   "destroy a function or a group of functions",
+			Action: func(c *cli.Context) error {
+				fmt.Println("down: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "list",
+			Aliases: []string{"l"},
+			Usage:   "list deployed services",
+			Action: func(c *cli.Context) error {
+				fmt.Println("list: ", c.Args().First())
+				return nil
+			},
+		},
 	}
 
-	version, help, verbose := parseFlags()
-	if help {
-		helpAndExit()
-	}
-
-	if version {
-		versionAndExit()
-	}
-
-	switch os.Args[1] {
-	case "serve":
-		server.Start(verbose)
-	case "up":
-		commands.Up()
-	case "down":
-		commands.Down()
-	case "list":
-		commands.List()
-	default:
-		fmt.Print(usage)
-		os.Exit(1)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
