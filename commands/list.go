0317b6777235b6b00ef12eaaa5e39a03b90ee9e4
Minghe Huang <h.minghe@gmail.com>
2018-06-20 10:47:11 +0800

    fix connect exception issue

--- commands/list.go
+++ commands/list.go
@@ -2,30 +2,26 @@ package commands
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/metrue/fx/api"
 	"github.com/metrue/fx/common"
-	"github.com/metrue/fx/config"
 	"github.com/pkg/errors"
 )
 
 // List lists all running function services
 func List(address string, functions []string) error {
-	client, conn, err := api.NewClient(config.GrpcEndpoint)
+	client, conn, err := api.NewClient(address)
 	if err != nil {
 		common.HandleError(err)
 		return errors.Wrap(err, "New client failed")
 	}
-
 	defer conn.Close()
-
-	fmt.Println(address, functions)
 
 	ctx := context.Background()
 	req := &api.ListRequest{
 		ID: functions,
 	}
+
 	res, err := client.List(ctx, req)
 	if err != nil {
 		common.HandleError(err)
