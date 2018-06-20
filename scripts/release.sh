27437a104a2616f41c75f9e6bb9d94e24bc771d6
Minghe Huang <h.minghe@gmail.com>
2018-06-20 16:13:04 +0800

    set name and email

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,5 +4,7 @@ set -e
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 commit=$(git rev-parse --verify HEAD)
+git config --global user.email "h.minghe@gmail.com"
+git config --global user.name "Minghe Huang"
 git tag -a v${branch}-${commit} -m "auto release"
 goreleaser --skip-validate
