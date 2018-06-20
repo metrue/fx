d73afe86662fb4414ae303423bc0eb6ad8694d63
Minghe Huang <h.minghe@gmail.com>
2018-06-20 16:37:19 +0800

    short commit id

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@
 set -e
 
 branch=$(git rev-parse --abbrev-ref HEAD)
-commit=$(git rev-parse --verify HEAD)
+commit=$(git rev-parse --short HEAD)
 git config --global user.email "h.minghe@gmail.com"
 git config --global user.name "Minghe Huang"
 git tag -a v${branch}-${commit} -m "auto release"
