fef248849cf6555e0480e9a48c1a3f37e84c3aa0
Minghe Huang <h.minghe@gmail.com>
2018-06-23 19:55:05 +0800

    bump version

--- fx.go
+++ fx.go
@@ -15,7 +15,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "fx"
 	app.Usage = "make function as a service"
-	app.Version = "0.0.4"
+	app.Version = "0.0.5"
 
 	app.Commands = []cli.Command{
 		{
