84aeba1ebbd5d7e3ea41922785f0eca6da4f9acd
Minghe Huang <h.minghe@gmail.com>
2018-06-24 00:20:11 +0800

    refactor config

--- commands/use.go
+++ commands/use.go
@@ -4,7 +4,5 @@ import "github.com/metrue/fx/config"
 
 func Use(address string) error {
 	config := config.GetConfig()
-	config.HttpServerAddr = address
-	config.GrpcEndpoint = address
-	return config.Save()
+	return config.SetHost(address)
 }
