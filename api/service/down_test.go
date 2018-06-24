9ff191d958a10108add4d411bf316350517235fc
Minghe Huang <h.minghe@gmail.com>
2018-06-24 17:17:35 +0800

    down works

--- api/service/down_test.go
+++ api/service/down_test.go
@@ -1,64 +1,15 @@
-package service
+package service_test
 
 import (
-	"context"
-	"errors"
 	"testing"
 
-	"github.com/metrue/fx/api"
+	. "github.com/metrue/fx/api/service"
+	"github.com/stretchr/testify/assert"
 )
 
-func containsFunction(client api.FxServiceClient, functionIDs ...string) (bool, int, error) {
-	ctx := context.Background()
-	listReq := &api.ListRequest{}
-	listRes, err := client.List(ctx, listReq)
-	if err != nil {
-		return false, 0, err
-	}
-	var found int
-	for _, fx := range listRes.Instances {
-		for _, fxID := range functionIDs {
-			if fx.FunctionID == fxID {
-				found++
-			}
-		}
-	}
-	if found != len(functionIDs) {
-		return false, found, errors.New("Missing functions")
-	}
-	return true, found, nil
-}
-
-func TestDownAll(t *testing.T) {
-
-	startServer()
-
-	client, conn, err := api.NewClient(grpcEndpoint)
-	defer stopServer(conn)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	fx1, err := createFunction(client)
-	if err != nil {
-		t.Fatalf("Up failed: %s\n", err.Error())
-	}
-	fx2, err := createFunction(client)
-	if err != nil {
-		t.Fatalf("Up failed: %s\n", err.Error())
-	}
-
-	if _, _, err := containsFunction(client, fx1.FunctionID, fx2.FunctionID); err != nil {
-		t.Fatalf(err.Error())
-	}
-
-	if _, err := removeFunction(client, "*"); err != nil {
-		t.Fatalf("Failed to remove functions: %s\n", err.Error())
-	}
-
-	if _, _, err := containsFunction(client); err != nil {
-		t.Fatalf(err.Error())
-	}
-
+func TestDown(t *testing.T) {
+	containerId := "hello-container-id-not-exist"
+	image := "world-image-name=not-exit"
+	_, err := DoDown(containerId, image)
+	assert.Equal(t, RemoveContainerError, err)
 }
