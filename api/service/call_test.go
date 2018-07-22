193b86e172f331c2b485b30dcf31591c7115db16
Minghe Huang <h.minghe@gmail.com>
2018-07-22 22:20:11 +0800

    add call endpoint

---
+++ api/service/call_test.go
@@ -0,0 +1,24 @@
+package service_test
+
+import (
+	"testing"
+
+	"github.com/metrue/fx/api"
+	. "github.com/metrue/fx/api/service"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCall(t *testing.T) {
+	req := &api.CallRequest{
+		Lang: "node",
+		Path: "./func.js",
+		Content: `module.exports = function (input) {
+	return parseInt(input.a, 10) + parseInt(input.b, 10)
+}
+		`,
+		Params: "a=1 b=4",
+	}
+	res, err := Call(nil, req)
+	assert.Nil(t, err)
+	assert.Equal(t, "5", res.Data)
+}
