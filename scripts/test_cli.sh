3aebff06eeed7bc8e4bb8b390dc09371d95d1209
Minghe Huang <h.minghe@gmail.com>
2019-03-13 23:28:21 +0800

    fix(typo): flag is name

--- scripts/test_cli.sh
+++ scripts/test_cli.sh
@@ -9,7 +9,7 @@ sleep 20 # waiting fx server to pulling resource done
 service='fx-service-abc'
 
 for lang in 'js' 'rb' 'py' 'go' 'php' 'jl' 'java' 'd'; do
-  ./build/fx up -t ${service} examples/functions/func.${lang} | grep 'info Run Service:'
+  ./build/fx up --name ${service} examples/functions/func.${lang} | grep 'info Run Service:'
   ./build/fx call examples/functions/func.js a=1 b=2 | grep '3'
   ./build/fx list | jq ''
   ./build/fx down ${service} | grep "Down Service ${service}"
