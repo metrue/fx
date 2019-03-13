f0ebc9dcec891d58427401de0834e613d5a88546
Minghe Huang <h.minghe@gmail.com>
2019-03-13 23:34:17 +0800

    chore(*): debug

--- scripts/test_cli.sh
+++ scripts/test_cli.sh
@@ -9,8 +9,8 @@ sleep 20 # waiting fx server to pulling resource done
 service='fx-service-abc'
 
 for lang in 'js' 'rb' 'py' 'go' 'php' 'jl' 'java' 'd'; do
-  ./build/fx up --name ${service} examples/functions/func.${lang} | grep 'info Run Service:'
-  ./build/fx call examples/functions/func.js a=1 b=2 | grep '3'
-  ./build/fx list | jq ''
-  ./build/fx down ${service} | grep "Down Service ${service}"
+  ./build/fx up --name ${service}_${lang} examples/functions/func.${lang} # | grep 'info Run Service:'
+  ./build/fx call examples/functions/func.js a=1 b=2 # | grep '3'
+  ./build/fx list # | jq ''
+  ./build/fx down ${service}_${lang} # | grep "Down Service ${service}"
 done
