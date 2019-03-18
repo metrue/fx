9dacbc1ee53e69a1a0974a753761cd7f6f3bc674
Minghe Huang <h.minghe@gmail.com>
2019-03-18 09:58:52 +0800

    refacot(api): seperate version

--- api/api.go
+++ api/api.go
@@ -34,42 +34,6 @@ type API struct {
 	box      packr.Box
 }
 
-// version get version of dockerd server
-func version(endpoint string) (string, error) {
-	path := "/version"
-	url := fmt.Sprintf("%s%s", endpoint, path)
-	if !strings.HasPrefix(url, "http") {
-		url = "http://" + url
-	}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return "", err
-	}
-	client := &http.Client{Timeout: 20 * time.Second}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-
-	if resp.StatusCode != 200 {
-		return "", fmt.Errorf("request %s failed: %d - %s", url, resp.StatusCode, resp.Status)
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	var res dockerTypes.Version
-	err = json.Unmarshal(body, &res)
-	if err != nil {
-		return "", err
-	}
-	return res.APIVersion, nil
-}
-
 // NewWithDockerRemoteAPI create a api with docker remote api
 func NewWithDockerRemoteAPI(url string) *API {
 	version, err := version(url)
