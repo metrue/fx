16ddee948675d0ac3f3b3808bb03ccd3d7fcc294
Minghe Huang <h.minghe@gmail.com>
2019-03-19 17:08:20 +0800

    feat(concurrent): down concurrent

--- api/down.go
+++ api/down.go
@@ -1,15 +1,28 @@
 package api
 
-import "github.com/apex/log"
+import (
+	"sync"
+
+	"github.com/apex/log"
+)
 
 // Down destroy services
 func (api *API) Down(names []string) error {
+	var wg sync.WaitGroup
+
 	for _, name := range names {
-		if err := api.Stop(name); err != nil {
-			log.Fatalf("Down Service %s: %v", name, err)
-		} else {
-			log.Infof("Down Service %s: \u2713", name)
-		}
+		wg.Add(1)
+		go func(s string) {
+			if err := api.Stop(s); err != nil {
+				log.Fatalf("Down Service %s: %v", s, err)
+			} else {
+				log.Infof("Down Service %s: \u2713", s)
+			}
+			defer wg.Done()
+		}(name)
 	}
+
+	wg.Wait()
+
 	return nil
 }
