efe7ad5b005df72257ae35c655bc87470d795d3f
Minghe <h.minghe@gmail.com>
2019-07-22 00:26:59 +0800

    run test concurrency (#208)
    * run test concurrency
    
    * enable cli test in PR

--- scripts/test_cli.sh
+++ scripts/test_cli.sh
@@ -2,13 +2,21 @@
 
 set -e
 
+fx="./build/fx"
 service='fx-service-abc'
 
-./build/fx init
+run() {
+  local lang=$1
+  $fx up --name ${service}_${lang} examples/functions/func.${lang}
+  $fx list # | jq ''
+  $fx down ${service}_${lang} # | grep "Down Service ${service}"
+}
+
+# main
+$fx init
+
 for lang in 'js' 'rb' 'py' 'go' 'php' 'jl' 'java' 'd' 'rs'; do
-  ./build/fx up --name ${service}_${lang} examples/functions/func.${lang} # | grep 'info Run Service:'
-  # when call the service, we have to make sure input params is correct (include correct type, since some statical language like Golang, it Unmashal payload into specific type)
-  # ./build/fx call examples/functions/func.${lang} a=1 b=2 # | grep '3'
-  ./build/fx list # | jq ''
-  ./build/fx down ${service}_${lang} # | grep "Down Service ${service}"
+  run $lang &
 done
+
+wait
