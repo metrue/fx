3e70e4c88b1af9339afbc2855eb0e2202ed39fc5
Minghe Huang <h.minghe@gmail.com>
2019-08-11 12:37:05 +0800

    check

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 ## install docker
-apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
 apt-get update -y
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common lsb-core
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common lsb-core
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable"
-apt-get update -y
-apt-get install -y docker-ce
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable"
+suod apt-get update -y
+sudo apt-get install -y docker-ce
+
 docker run hello-world
 
 ## start fx proxy agent
