aa4ba0768863f5793b68c9380207ca45d43149b9
Minghe Huang <h.minghe@gmail.com>
2019-08-11 12:44:56 +0800

    check

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -3,7 +3,7 @@
 ## install docker
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
 apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common lsb-core
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common lsb-core curl
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable"
 suod apt-get update -y
