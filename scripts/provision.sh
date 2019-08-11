2fd4abbe9f5eb4c12b6a97d8d6a611b0f5aad442
Minghe Huang <h.minghe@gmail.com>
2019-08-11 12:33:25 +0800

    start fx agent

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+## install docker
 apt-get remove -y docker docker-engine docker.io containerd runc
 apt-get update -y
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common lsb-core
@@ -8,3 +9,6 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$
 apt-get update -y
 apt-get install -y docker-ce
 docker run hello-world
+
+## start fx proxy agent
+docker run -d --name=fx-agent --rm -v /var/run/docker.sock:/var/run/docker.sock -p 0.0.0.0:8866:1234 bobrik/socat TCP-LISTEN:1234,fork UNIX-CONNECT:/var/run/docker.sock
