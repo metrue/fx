bea48d26b37ec578a2f4306975bc432eff20fb07
Minghe <h.minghe@gmail.com>
2019-09-27 17:36:43 +0800

    setup kind and k8s (#269)

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -11,5 +11,11 @@ sudo apt-get install -y docker-ce
 
 docker run hello-world
 
+# curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+# mkdir -p ${HOME}/.kube
+# touch ${HOME}/.kube/confi
+#
+
 ## start fx proxy agent
 docker run -d --name=fx-agent --rm -v /var/run/docker.sock:/var/run/docker.sock -p 0.0.0.0:8866:1234 bobrik/socat TCP-LISTEN:1234,fork UNIX-CONNECT:/var/run/docker.sock
+
