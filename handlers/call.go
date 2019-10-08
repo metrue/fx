0c70beb41d8e380ada00ee9f27962f0de0f6c5cb
Minghe <h.minghe@gmail.com>
2019-10-08 22:41:44 +0800

    move docker HTTP client code into container_runtimes folder

--- handlers/call.go
+++ handlers/call.go
@@ -5,9 +5,9 @@ import (
 	"strings"
 
 	"github.com/apex/log"
-	"github.com/metrue/fx/api"
 	"github.com/metrue/fx/config"
 	"github.com/metrue/fx/constants"
+	api "github.com/metrue/fx/container_runtimes/docker/http"
 	"github.com/metrue/fx/packer"
 	"github.com/metrue/fx/types"
 	"github.com/metrue/fx/utils"
