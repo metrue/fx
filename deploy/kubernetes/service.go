7ba72358f0ed5c24b68b256d8f6d6a4759f5a8ba
Minghe Huang <h.minghe@gmail.com>
2019-10-12 16:14:14 +0800

    * create a deployment instead of create a pod directly * remove host port expose to fix the port ocuppied issue

--- deploy/kubernetes/service.go
+++ deploy/kubernetes/service.go
@@ -1,11 +1,57 @@
 package kubernetes
 
 import (
+	"strconv"
+
 	"github.com/metrue/fx/constants"
-	v1 "k8s.io/api/core/v1"
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	intstr "k8s.io/apimachinery/pkg/util/intstr"
 )
+
+func generateServiceSpec(
+	namespace string,
+	name string,
+	typ string,
+	ports []int32,
+	selector map[string]string,
+) *apiv1.Service {
+	servicePorts := []apiv1.ServicePort{
+		apiv1.ServicePort{
+			Name:       "http",
+			Protocol:   apiv1.ProtocolTCP,
+			Port:       80,
+			TargetPort: intstr.FromInt(int(constants.FxContainerExposePort)),
+		},
+		apiv1.ServicePort{
+			Name:       "https",
+			Protocol:   apiv1.ProtocolTCP,
+			Port:       443,
+			TargetPort: intstr.FromInt(int(constants.FxContainerExposePort)),
+		},
+	}
+	// Append custom Port
+	for index, port := range ports {
+		servicePorts = append(servicePorts, apiv1.ServicePort{
+			Name:       "custom-port-" + strconv.Itoa(index),
+			Protocol:   apiv1.ProtocolTCP,
+			Port:       port,
+			TargetPort: intstr.FromInt(int(3000)),
+		})
+	}
+
+	return &apiv1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			ClusterName: namespace,
+		},
+		Spec: apiv1.ServiceSpec{
+			Ports:    servicePorts,
+			Type:     apiv1.ServiceType(typ),
+			Selector: selector,
+		},
+	}
+}
 
 // CreateService create a service
 func (k *K8S) CreateService(
@@ -13,44 +59,9 @@ func (k *K8S) CreateService(
 	name string,
 	typ string,
 	ports []int32,
-	podsLabels map[string]string,
-) (*v1.Service, error) {
-	servicePorts := []v1.ServicePort{
-		v1.ServicePort{
-			Name:       "http",
-			Protocol:   v1.ProtocolTCP,
-			Port:       80,
-			TargetPort: intstr.FromInt(int(constants.FxContainerExposePort)),
-		},
-		v1.ServicePort{
-			Name:       "https",
-			Protocol:   v1.ProtocolTCP,
-			Port:       443,
-			TargetPort: intstr.FromInt(int(constants.FxContainerExposePort)),
-		},
-	}
-	// Append custom Port
-	for _, port := range ports {
-		servicePorts = append(servicePorts, v1.ServicePort{
-			Name:       "custom",
-			Protocol:   v1.ProtocolTCP,
-			Port:       port,
-			TargetPort: intstr.FromInt(int(3000)),
-		})
-	}
-
-	service := &v1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:        name,
-			ClusterName: namespace,
-		},
-		Spec: v1.ServiceSpec{
-			Ports:    servicePorts,
-			Type:     v1.ServiceType(typ),
-			Selector: podsLabels,
-		},
-	}
-
+	selector map[string]string,
+) (*apiv1.Service, error) {
+	service := generateServiceSpec(namespace, name, typ, ports, selector)
 	createdService, err := k.CoreV1().Services(namespace).Create(service)
 	if err != nil {
 		return nil, err
@@ -59,9 +70,32 @@ func (k *K8S) CreateService(
 	return createdService, nil
 }
 
+// UpdateService update a service
+// TODO this method is not perfect yet, should refactor later
+func (k *K8S) UpdateService(
+	namespace string,
+	name string,
+	typ string,
+	ports []int32,
+	selector map[string]string,
+) (*apiv1.Service, error) {
+	svc, err := k.GetService(namespace, name)
+	if err != nil {
+		return nil, err
+	}
+	svc.Spec.Selector = selector
+	svc.Spec.Type = apiv1.ServiceType(typ)
+	return k.CoreV1().Services(namespace).Update(svc)
+}
+
 // DeleteService a service
 func (k *K8S) DeleteService(namespace string, name string) error {
 	// TODO figure out the elegant way to delete a service
 	options := &metav1.DeleteOptions{}
 	return k.CoreV1().Services(namespace).Delete(name, options)
+}
+
+// GetService get a service
+func (k *K8S) GetService(namespace string, name string) (*apiv1.Service, error) {
+	return k.CoreV1().Services(namespace).Get(name, metav1.GetOptions{})
 }
