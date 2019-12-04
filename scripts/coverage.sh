3219424fc94ddda840cd8247d4149a6d63c1938c
Minghe Huang <h.minghe@gmail.com>
2019-12-04 08:18:21 +0800

    combine coverage and unit test progres

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,6 +5,9 @@ echo "mode: atomic\n" > coverage.txt
 for d in `go list ./... | grep -v 'mocks\|images\|examples\|assets'`; do
   echo $d
   go test -race -coverprofile=profile.out -covermode=atomic $d
+  if [ $? -ne 0 ];then
+    exit 1
+  fi
   if [ -f profile.out ]; then
       cat profile.out | grep -v "mode: atomic" >> coverage.txt
       rm profile.out
