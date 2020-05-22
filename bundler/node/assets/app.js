609af8d96bde06e4bfb799964993ec1fbf1819e3
Minghe Huang <h.minghe@gmail.com>
2020-05-22 14:19:24 +0800

    adopt the swagger-stats as API operation metrics collection tool.
    FYI: https://github.com/slanatech/swagger-stats

--- bundler/node/assets/app.js
+++ bundler/node/assets/app.js
@@ -1,9 +1,13 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
+const swStats = require('swagger-stats');
+const e2k = require('express-to-koa');
 const fx = require('./fx');
 
 const app = new Koa();
+
+app.use(e2k(swStats.getMiddleware({})));
 app.use(cors({
   origin: '*',
 }));
