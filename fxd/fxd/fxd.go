1abf063d61d235e7c3497ef7a8fb89d65f320eb1
Minghe Huang <h.minghe@gmail.com>
2020-06-13 23:41:01 +0800

    works

--- fxd/fxd/fxd.go
+++ fxd/fxd/fxd.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+
+	dockerSDK "github.com/metrue/fx/container_runtimes/docker/sdk"
+	"github.com/metrue/fx/driver"
+	dockerDriver "github.com/metrue/fx/driver/docker"
 
 	"github.com/metrue/fx/api"
 	"google.golang.org/grpc"
 )
 
 type server struct {
+	driver driver.Driver
 }
 
 func (s *server) ListService(ctx context.Context, in *api.ListServiceRequest) (*api.ListServiceResponse, error) {
+	filter := in.GetFilter()
+	fmt.Println("++++++++")
+	fmt.Println(filter)
+	fmt.Println("++++++++")
+	services, err := s.driver.List(ctx, filter.GetName())
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(services)
 	return &api.ListServiceResponse{}, nil
 }
 
@@ -26,7 +41,16 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	api.RegisterAPIServer(s, &server{})
+
+	docker, err := dockerSDK.CreateClient(context.Background())
+	if err != nil {
+		log.Fatalf("failed to create a docker cli: %v", err)
+	}
+
+	driver := dockerDriver.New(dockerDriver.Options{
+		DockerClient: docker,
+	})
+	api.RegisterAPIServer(s, &server{driver: driver})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
